003451057eaff99928ff7674009de8165957ed06
Kevin Buffardi <kbuffardi@csuchico.edu>
2026-03-01 20:30:20 -0800

    Repeats prompt until correct response; close #2

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 
 int main(){
   std::string input;
-  std::cout<<"Hola.\n";
-  std::cin>>input;
-  
+
+  do{
+    std::cout<<"Hola.\n";
+    std::cin>>input;
+  }while( input != "hola" );
+
+  std::cout << "gracias\n";
+
   return 0;
 }
